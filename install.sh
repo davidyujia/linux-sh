fa523736cf7c061dc14ad7fdc234da3eeb937927
U+ <16407936+davidyujia@users.noreply.github.com>
2020-09-12 13:09:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
-sudo apt update && sudo apt -y upgrade \
+sudo su
+apt update && apt -y upgrade \
 # install node.js
-sudo apt install nodejs npm -y && \
-sudo npm i -g n -y && \
-sudo n latest && \
-sudo apt remove nodejs npm -y \
+apt install nodejs npm -y && \
+npm i -g n -y && \
+n latest && \
+apt remove nodejs npm -y \
 # install angular
-sudo npm install -g @angular/cli && \
+npm install -g @angular/cli && \
 # install dotnet core
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && \
-sudo dpkg -i packages-microsoft-prod.deb && \
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-3.1 && \
+dpkg -i packages-microsoft-prod.deb && \
+apt-get install -y apt-transport-https && \
+apt-get update && \
+apt-get install -y dotnet-sdk-3.1 && \
 # install docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io && \
-sudo service docker start && \
-sudo mkdir -p /data/portainer && \
-sudo docker run -d --restart=unless-stopped --network host --name portainer -h portainer -v /var/run/docker.sock:/var/run/docker.sock -v /data/portainer:/data portainer/portainer
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get install docker-ce docker-ce-cli containerd.io && \
+# docker remote setting
+su && \
+echo "DOCKER_OPTS='-H unix:// -H tcp://127.0.0.1:2375'" >> /etc/default/docker && \
+# start docker
+service docker start && \
+mkdir -p /data/portainer && \
+docker run -d --restart=unless-stopped --network host --name portainer -h portainer -v /var/run/docker.sock:/var/run/docker.sock -v /data/portainer:/data portainer/portainer
