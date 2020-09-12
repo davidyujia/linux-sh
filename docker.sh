31027a59d475df7a42e6c90932e5e88dbcad22e7
U+ <16407936+davidyujia@users.noreply.github.com>
2020-09-12 14:32:18 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 # sudo nano /etc/default/docker
 # DOCKER_OPTS="-H unix:// -H tcp://127.0.0.1:2375"
+# echo "DOCKER_OPTS='-H unix:// -H tcp://127.0.0.1:2375'" >> /etc/default/docker && \
 
 sudo service docker start
