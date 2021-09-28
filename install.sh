9456a217b851955bae99ecb72fc59827700b4b69
U+ <16407936+davidyujia@users.noreply.github.com>
2021-09-28 14:06:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,4 @@ wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb && \
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-5.0 && \
-# install docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io && \
-echo "DOCKER_OPTS=\"-H unix:// -H tcp://127.0.0.1:2375\"" | sudo tee -a /etc/default/docker && \
-sudo service docker start && \
-sudo mkdir -p /data/portainer && \
-sudo docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo apt-get install -y dotnet-sdk-5.0
