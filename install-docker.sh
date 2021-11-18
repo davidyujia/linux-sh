480b28db12d1d021a569c8720e12c0a04504aa89
U+ <16407936+davidyujia@users.noreply.github.com>
2021-11-18 11:22:38 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io && \
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io && \
 echo "DOCKER_OPTS=\"-H unix:// -H tcp://127.0.0.1:2375\"" | sudo tee -a /etc/default/docker && \
 sudo service docker start && \
 sudo mkdir -p /data/portainer && \
