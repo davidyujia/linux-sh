f5fc486567e183a34049499040eb6cd1d8748426
David <16407936+davidyujia@users.noreply.github.com>
2022-05-03 17:19:25 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,4 @@
 # L2TP over IPsec 
-sudo add-apt-repository ppa:nm-l2tp/network-manager-l2tp \
-sudo apt-get update \
-sudo apt-get install network-manager-l2tp network-manager-l2tp-gnome
+sudo add-apt-repository -y ppa:nm-l2tp/network-manager-l2tp && \
+sudo apt-get update && \
+sudo apt-get install network-manager-l2tp network-manager-l2tp-gnome -y
