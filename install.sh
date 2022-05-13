cb6e519c0521a4ea97ba0f5207aa015f49c82b80
David <16407936+davidyujia@users.noreply.github.com>
2022-05-14 04:03:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,3 @@ sudo dpkg -i packages-microsoft-prod.deb && \
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y dotnet-sdk-6.0
-# install podman
-. /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install podman
-alias docker=podman
