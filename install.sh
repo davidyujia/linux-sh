f09a5c4500913834cd6135dac36df2a0953f65c8
David <16407936+davidyujia@users.noreply.github.com>
2022-08-05 14:01:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,4 @@ sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
 sudo apt-get install -y dotnet-sdk-6.0
 # WOL tool
-sudo apt-get install wakeonlan
+# sudo apt-get install wakeonlan
