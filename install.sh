3f8a32296c0e762db461deda75f5c4506899b3db
David <16407936+davidyujia@users.noreply.github.com>
2022-08-20 11:31:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,6 @@ sudo apt remove nodejs npm -y \
 # install angular
 sudo npm install -g @angular/cli && \
 # install dotnet core
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && \
-sudo dpkg -i packages-microsoft-prod.deb && \
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get install -y dotnet6
 # WOL tool
 # sudo apt-get install wakeonlan
