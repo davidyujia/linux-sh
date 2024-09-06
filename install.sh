6a11f519a306c8c89d90fac1366abcd48887195a
David <16407936+davidyujia@users.noreply.github.com>
2024-09-07 01:02:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,6 @@ sudo apt autoremove -y \
 # WOL tool
 # sudo apt-get install wakeonlan
 # Install Flatpak
-sudo apt install flatpak -y
-sudo apt install gnome-software-plugin-flatpak -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# sudo apt install flatpak -y
+# sudo apt install gnome-software-plugin-flatpak -y
+# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
